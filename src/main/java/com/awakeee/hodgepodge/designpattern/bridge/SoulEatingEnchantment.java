0a97ea03b2f4faf795f067c61b574899d30fe9cf
wakeee <172254104@qq.com>
2019-03-20 16:52:24 +0800

    桥接模式

---
+++ src/main/java/com/awakeee/hodgepodge/designpattern/bridge/SoulEatingEnchantment.java
@@ -0,0 +1,18 @@
+package com.awakeee.hodgepodge.designpattern.bridge;
+
+public class SoulEatingEnchantment implements Enchantment {
+    @Override
+    public void onActivate() {
+        System.out.println("The item spreads bloodust");
+    }
+
+    @Override
+    public void apply() {
+        System.out.println("The item eats the soul of enemies");
+    }
+
+    @Override
+    public void onDeactivate() {
+        System.out.println("Bloodust slowly disappears");
+    }
+}
---
+++ src/main/java/com/awakeee/hodgepodge/designpattern/bridge/Weapon.java
@@ -0,0 +1,10 @@
+package com.awakeee.hodgepodge.designpattern.bridge;
+
+public interface Weapon {
+
+    void wield();//装备武器
+
+    void swing();//挥舞武器
+
+    void unwield();//卸下武器
+}
